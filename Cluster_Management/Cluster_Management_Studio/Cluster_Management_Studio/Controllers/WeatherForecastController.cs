01b932c6a1fbb869981eafe182c314ff1e1ea198
Daniel <Daniel@laptop>
2022-01-10 21:19:15 +0100

    Created Blazor API (ASP.NET Core) with Dockerfile inside

---
+++ Cluster_Management/Cluster_Management_Studio/Cluster_Management_Studio/Controllers/WeatherForecastController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Cluster_Management_Studio.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var rng = new Random();
+            return Ok(rng.Next(10, 100));
+        }
+    }
+}
